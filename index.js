341bc9f8bcfc84acbc39bf640648d8314fec59df
Shajedul-Shazid <programmershajid@gmail.com>
2024-01-13 18:59:54 +0600

    pushing data into the firebase database

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-344ad-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "todo")
+
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value    
-    console.log(inputValue)
+    let inputValue = inputFieldEl.value  
+
+    push(moviesInDB, inputValue)
+
+    console.log(`${inputValue} added to the database`)
     inputFieldEl.value = ""
 })
+
