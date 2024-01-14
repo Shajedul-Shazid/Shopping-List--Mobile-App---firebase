04367fb75905656610492939ad2e9e92fb7ea2a5
Shajedul-Shazid <programmershajid@gmail.com>
2024-01-14 09:59:20 +0600

    get data from the firebase database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-344ad-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -9,18 +9,33 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const moviesInDB = ref(database, "todo")
 
-
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
+const shoppingListEl = document.getElementById("shopping-list")
+
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value  
-
-    push(moviesInDB, inputValue)
-
-    console.log(`${inputValue} added to the database`)
-    inputFieldEl.value = ""
+    push(moviesInDB, inputValue)    
+    clearInputField()    
 })
 
-trying to upadte 
+onValue(moviesInDB, function(snapshot) {
+    let itemsArray = Object.values( snapshot.val() )
+     
+    shoppingListEl.innerHTML = ""
 
+    for( let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+        addShoppingListEl(currentItem)
+    } 
+
+})
+
+function clearInputField() {
+    inputFieldEl.value = ""
+}
+
+function addShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML +=`<li> ${itemValue} </li>`
+}
